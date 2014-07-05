13c6ccfcd9f0aedd50f21841cfb0147dcf739abd
Brian Ford <btford@umich.edu>
2014-07-05 10:30:42 -0700

    refactor: the whole thing

--- debounce.js
+++ debounce.js
@@ -1,29 +1,29 @@
 module.exports = function debounceOn (fn, timeout, hash) {
   var timeouts = {};
 
-  timeout = timeout || 100;
-
-  hash = hash || defaultHash;
+  timeout = typeof timeout === 'number' ? timeout : (hash = timeout, 100);
+  hash = typeof hash === 'function' ? hash : defaultHash;
 
   return function () {
     var key = hash.apply(null, arguments);
     var args = arguments;
-    if (!timeouts[key]) {
+    if (typeof timeouts[key] === 'undefined') {
       timeouts[key] = setTimeout(function () {
         delete timeouts[key];
         fn.apply(null, args);
       }, timeout);
-
-      return function cancel () {
-        return timeouts[key] && (clearTimeout(timeouts[key]), true);
-      };
     }
-    return noop;
-  }
-}
+    return function cancel () {
+      if (timeouts[key]) {
+        clearTimeout(timeouts[key]);
+        delete timeouts[key];
+        return true;
+      }
+      return false;
+    };
+  };
+};
 
 function defaultHash () {
-  return Array.prototype.join.map(arguments, '::');
+  return Array.prototype.join.call(arguments, '::');
 }
-
-function noop () {}
